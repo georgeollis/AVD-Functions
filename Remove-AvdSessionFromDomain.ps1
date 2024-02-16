813d4ef2a45b61c52c1b0ee75e7370baecbcd920
George Ollis <76496924+georgeollis@users.noreply.github.com>
2024-02-16 15:23:53 +0000

    Create Remove-AvdSessionFromDomain.ps1

---
+++ Remove-AvdSessionFromDomain.ps1
@@ -0,0 +1,21 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)][string]$DomainUserName,
+    [Parameter(Mandatory = $true)][string]$DomainPassword
+)
+
+$root = "LDAP://$((Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem | Select-Object Name, Domain).Domain)"
+
+try { 
+    $domain = New-Object System.DirectoryServices.DirectoryEntry($root, $DomainUserName, $DomainPassword) -ErrorAction Stop
+    $search = New-Object -TypeName System.DirectoryServices.DirectorySearcher($domain) -ErrorAction Stop
+}
+catch {
+    Write-Error "Error: unable to create new objects. $($_.Exception.Message)"
+}
+
+$search.filter = "(&(ObjectCategory=Computer)(ObjectClass=Computer)((cn=$($env:computername))))" 
+$computer = $search.FindOne()
+$dnc = $computer.GetDirectoryEntry()                           
+$dnc = $computer.GetDirectoryEntry()
+$dnc.DeleteTree()
